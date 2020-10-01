dc7af674f73f14a666e0417609626a6214bea9e3
Folu-1 <65354203+Folu-1@users.noreply.github.com>
2020-09-30 20:37:47 -0400

    Add files via upload

---
+++ extras.py
@@ -0,0 +1,10 @@
+#print('Importing extras into Main.py')
+
+try:
+    import helper
+except Exception as e:
+    print(f"Error importing file, here is the error: {e}")
+
+#print(f"__name__ in extras.py: {__name__}")
+
+name = 'MoFiyinfoluwa Tokode'
